0a24b7d259cfc4c82a6beff1f9c2c97c03fdc916
Kacper <95192193+ElGoblin0@users.noreply.github.com>
2022-01-11 22:51:28 +0100

    Add files via upload

---
+++ write.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include <fstream>
+
+#include <hwd.h>
+
+#include "common.h"
+
+
+
+int main(int argc, char * argv[])
+try
+{
+    std::vector<char> data;
+    std::vector<char> data2;
+    std::vector<char> temp;
+    
+    if (argc > 1)
+    {
+        auto input = safely_open_file(argv[1]);
+        data = read_entire_stream(input);
+
+        auto input2 = safely_open_file(argv[2]);
+        data2 = read_entire_stream(input2);
+
+        temp.reserve(data.size() + data2.size());
+        temp.insert(temp.end(), data.begin(), data.end());
+        temp.insert(temp.end(), data2.begin(), data2.end());
+    }
+    else
+    {
+        data = read_entire_stream(std::cin);
+    }   
+
+    if(data.empty())
+    {
+        throw std::runtime_error("nothing to write");
+    }
+
+    bool ok = hwd::memory::write(temp, 0);
+
+    if(!ok)
+    {
+        throw std::runtime_error("could not write");
+    }
+}
+catch (const std::runtime_error & err)
+{
+    std::cerr << err.what() << std::endl;
+    return EXIT_FAILURE;
+}
