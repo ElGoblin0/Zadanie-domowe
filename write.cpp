9a46cc07dcefcad8314c7f315509d1a61c7e037e
Kacper <95192193+ElGoblin0@users.noreply.github.com>
2022-01-12 17:33:19 +0100

    Update write.cpp

--- write.cpp
+++ write.cpp
@@ -15,19 +15,26 @@ try
     std::vector<char> data;
     std::vector<char> data2;
     std::vector<char> temp;
+    std::vector<char> end_of_file;
+    end_of_file.push_back('e');
+    end_of_file.push_back('o');
+    end_of_file.push_back('f');
+    end_of_file.push_back('\n');
+
     
     if (argc > 1)
     {
         auto input = safely_open_file(argv[1]);
         data = read_entire_stream(input);
 
-        if(argv[2] != 0)
+        if(argv[2] != nullptr)
         {
             auto input2 = safely_open_file(argv[2]);
             data2 = read_entire_stream(input2);
         
-            temp.reserve(data.size() + data2.size());
+            temp.reserve(data.size() + data2.size() + end_of_file.size());
             temp.insert(temp.end(), data.begin(), data.end());
+            temp.insert(temp.end(), end_of_file.begin(), end_of_file.end());
             temp.insert(temp.end(), data2.begin(), data2.end());
         }
         else
